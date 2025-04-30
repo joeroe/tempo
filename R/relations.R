d10ee7a792a62b2b3baace8dc26273a5674f5774
Joe Roe <git@joeroe.io>
2025-04-30 10:30:54 +0200

    Export and document relations

--- R/relations.R
+++ R/relations.R
@@ -1,116 +1,191 @@
 # relations.R
-# Temporal relations between periods
-#
-# References:
-# - Levy et al. 2021 <https://doi.org/10.1016/j.jas.2020.105225> 
-# - Levy in press.
+# Logical functions for temporal relations between periods (Levy's typology)
 
+
+#' Temporal relations
+#' 
+#' These functions test for the logical relation between two periods according
+#' to Levy's typology (Levy et al. 2021 <https://doi.org/10.1016/j.jas.2020.105225>;
+#' Levy in press).
+#'
+#' @param x,y Pair of periods to test the relation between, each specified as a 
+#'   two-element numeric vector with start and end times.
+#' @param strict By default, comparison is inclusive (i.e. using `<=` and `>=`).
+#'   Use `strict = FALSE` for strict comparison (i.e. using `<` and `>`).
+#'
+#' @return
+#' Logical vector the same length as `x` and `y`.
+#'
+#' @name relations
+#'
+#' @examples
+#' period1 <- c(1500, 1900)
+#' period2 <- c(1800, 1950)
+#' period3 <- c(1900, 1950)
+#' 
+#' contemporary_with(period1, period2)
+#' 
+#' # Inclusive relations (the default)
+#' contemporary_with(period1, period3)
+#' 
+#' # Strict relations
+#' contemporary_with(period1, period3, strict = TRUE)
+NULL
+
+#' @rdname relations
+#' @export
 starts_before_end_of <- function(x, y, strict = FALSE) {
   lt(start_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 ends_after_start_of <- function(x, y, strict = FALSE) {
   gt(start_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 starts_before_start_of <- function(x, y, strict = FALSE) {
   lt(start_of(x), start_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 starts_after_start_of <- function(x, y, strict = FALSE) {
   gt(start_of(x), start_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 ends_before_end_of <- function(x, y, strict = FALSE) { 
   lt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 ends_after_end_of <- function(x, y, strict = FALSE) { 
   gt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 ends_before_start_of <- function(x, y, strict = FALSE) {
   lt(end_of(x), start_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 starts_after_end_of <- function(x, y, strict = FALSE) { 
   gt(start_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 meets <- function(x, y, strict = FALSE) {
   end_of(x) == start_of(y)
 }
 
+#' @rdname relations
+#' @export
 met_by <- function(x, y, strict = FALSE) {
   start_of(x) == end_of(y)
 }
 
+#' @rdname relations
+#' @export
 contemporary_with <- function(x, y, strict = FALSE) {
   gt(end_of(x), start_of(y), strict) & 
     lt(start_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 starts_during <- function(x, y, strict = FALSE) {
   lt(start_of(y), start_of(x), strict) &
     lt(start_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 includes_start_of <- function(x, y, strict = FALSE) { 
   lt(start_of(x), start_of(y), strict) &
     lt(start_of(y), end_of(x), strict)
 }
 
+#' @rdname relations
+#' @export
 starts_with <- function(x, y, strict = FALSE) { 
   start_of(x) == start_of(y)
 }
 
+#' @rdname relations
+#' @export
 ends_with <- function(x, y, strict = FALSE) {
   end_of(x) == end_of(y)
 }
 
+#' @rdname relations
+#' @export
 overlaps_before <- function(x, y, strict = FALSE) { 
   lt(start_of(x), start_of(y), strict) &
     lt(start_of(y), end_of(x), strict) &
     lt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 overlaps_after <- function(x, y, strict = FALSE) { 
   lt(start_of(y), start_of(x), strict) &
     lt(start_of(x), end_of(y), strict) &
     lt(end_of(y), end_of(x), strict)
 }
 
+#' @rdname relations
+#' @export
 includes <- function(x, y, strict = FALSE) { 
   lt(start_of(x), start_of(y), strict) &
     gt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 included_in <- function(x, y, strict = FALSE) { 
   gt(start_of(x), start_of(y), strict) &
     lt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 begins <- function(x, y, strict = FALSE) { 
   (start_of(x) == start_of(y)) &
     lt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 begun_by <- function(x, y, strict = FALSE) { 
   (start_of(x) == start_of(y)) &
     gt(end_of(x), end_of(y), strict)
 }
 
+#' @rdname relations
+#' @export
 ends <- function(x, y, strict = FALSE) { 
   (end_of(x) == end_of(y)) &
     gt(start_of(x), start_of(y))
 }
 
+#' @rdname relations
+#' @export
 ended_by <- function(x, y, strict = FALSE) { 
   (end_of(x) == end_of(y)) &
     lt(start_of(x), start_of(y))
 }
 
-# N.B. `equal_to` not `equals` to avoid conflict with `magrittr::equals`
+#' @rdname relations
+#' @export
+#  N.B. `equal_to` not `equals` to avoid conflict with `magrittr::equals`
 equal_to <- function(x, y, strict = FALSE) { 
   (start_of(x) == start_of(y)) &
     (end_of(x) == end_of(y))
--- README.md
+++ README.md
@@ -26,3 +26,37 @@ You can install the development version of tempo from GitHub with the
 ``` r
 remotes::install_github("joeroe/tempo")
 ```
+
+## Example
+
+Test whether two periods, specified as numeric vectors of start and end
+dates, are contemporary with each other:
+
+``` r
+library("tempo")
+#> Loading required package: rlang
+
+period1 <- c(1500, 1900)
+period2 <- c(1800, 1950)
+
+contemporary_with(period1, period2)
+#> [1] TRUE
+```
+
+tempo supports fifteen types of temporal relations following Levy’s
+typology (Levy et al. 2021 <https://doi.org/10.1016/j.jas.2020.105225>;
+Levy et al. in press). See `?relations` for a list.
+
+By default, comparison of start and end points is inclusive (i.e. using
+`>=` and `<=`). `strict = TRUE` enables the ‘strict’ variants of each
+relation (i.e. using `>` and `<`):
+
+``` r
+period3 <- c(1900, 1950)
+
+contemporary_with(period1, period3)
+#> [1] TRUE
+
+contemporary_with(period1, period3, strict = TRUE)
+#> [1] FALSE
+```
